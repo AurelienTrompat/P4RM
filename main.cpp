cfa497c03cb6d4e27b0a5f55d55eaf412e799b5b
DjNounours <aurelien.trompat41@orange.fr>
2016-10-22 10:10:11 +0200

    Ajout du projet avec la classe PGpio
    Faut bien commencer quelque part

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <unistd.h>
+#include "Pgpio.hpp"
+
+using namespace std;
+
+int main()
+{
+    PGpio led(26, true), sw(21, false);
+    while(!sw.read())
+    {
+        led.write(1);
+        usleep(100000);
+        led.write(0);
+        usleep(100000);
+    }
+    return 0;
+}
---
+++ src/Pgpio.cpp
@@ -0,0 +1,50 @@
+#include "PGpio.hpp"
+
+using namespace std;
+
+PGpio::PGpio(const uint8_t pin, const bool direction)
+{
+    mDirection=direction;
+    if(pin >= 4 && pin <= 27)
+        mPin=pin;
+    else
+        mPin=0;
+
+    mFile.open("/sys/class/gpio/export",ios::out);
+    mFile<<to_string(mPin);
+    mFile.close();
+    mFile.open("/sys/class/gpio/gpio"+to_string(mPin)+"/direction",ios::out);
+    if(mDirection)
+        mFile<<"out";
+    else
+        mFile<<"in";
+    mFile.close();
+
+}
+
+PGpio::~PGpio()
+{
+    mFile.open("/sys/class/gpio/unexport",ios::out);
+    mFile<<to_string(mPin);
+    mFile.close();
+}
+
+bool PGpio::read()
+{
+    bool value;
+    mFile.open("/sys/class/gpio/gpio" + to_string(mPin) +"/value",ios::in);
+    mFile>>value;
+    mFile.close();
+    return value;
+}
+
+void PGpio::write(const bool value)
+{
+    if(mDirection)
+    {
+        mFile.open("/sys/class/gpio/gpio"+to_string(mPin)+"/value",ios::out);
+        mFile<<to_string(value);
+        mFile.close();
+    }
+}
+
